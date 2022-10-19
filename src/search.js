061118d7fcc5a4f89d32b9d9c0a10cfe1835700b
king-lox2 <benedicteverist00@gmail.com>
2022-10-19 10:22:29 +0100

    initial commit

--- src/search.js
+++ src/search.js
@@ -2,7 +2,7 @@ import showData from './showData.js';
 
 const cocktailApi = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'; 
 
-const formApi = 'https:www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const formApi = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 const form = document.querySelector('.form-search');
 
