a8ee40ccba09609213c0fc5ec7545fe54395c465
king-lox2 <benedicteverist00@gmail.com>
2022-10-19 10:14:26 +0100

    initial commit

--- src/fetchData.js
+++ src/fetchData.js
@@ -14,7 +14,7 @@ cocktailContainer.innerHTML = `
 `;
   try {
  const response = await fetch(url);
- const data = await await response.json();
+ const data = await response.json();
  
   return data;
   } 
